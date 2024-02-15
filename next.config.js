d1fa1e94d49e4ec493a88423bdf37ce77464f89c
Ashfak Niloy <ashfakniloy56@gmail.com>
2024-02-15 20:14:03 +0600

    added headers setting in config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/blog/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "*",
+          },
+        ],
+      },
+    ];
+  },
+
   images: {
     remotePatterns: [
       {
