5ecddc8e79efac05bd3b4bf17b116bc8505636be
Ashfak Niloy <ashfakniloy56@gmail.com>
2024-02-15 18:34:56 +0600

    updated nextjs

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,4 +1,4 @@
-import { Metadata } from "next";
+// import { Metadata } from "next";
 import Image from "next/image";
 import { Section } from "@/components/Wrapper/Section";
 import { getFomrattedDate } from "@/utils/getFomrattedDate";
