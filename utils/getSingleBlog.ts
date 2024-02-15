74a7d3487399c1d7d123a6d27570f95e87a1a02e
Ashfak Niloy <ashfakniloy56@gmail.com>
2024-02-15 22:11:14 +0600

    testing alternative approach for sending headers

--- utils/getSingleBlog.ts
+++ utils/getSingleBlog.ts
@@ -1,18 +1,24 @@
 import { API_URL } from "@/config";
 import { headers } from "next/headers";
 
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "Access-Control-Allow-Headers": "*",
+};
+
 export async function getSingleBlog({
   slug,
 }: {
   slug: string;
 }): Promise<BlogProps | null> {
-  const headerList = headers();
+  // const headerList = headers();
 
-  // console.log("headerList", headerList.get("x-hello-from-middleware"));
+  // console.log("headerx", corsHeaders);
 
   const res = await fetch(`${API_URL}/blogs/${slug}`, {
     cache: "no-store",
-    headers: headers(), // error after deploying to vercel
+    headers: corsHeaders, // error after deploying to vercel
   });
 
   const data = await res.json();
